26d17f8fe05ca4ae295e7b4531a5d246e5bc2a49
matinty <az1387928@gmail.com>
2025-12-04 22:12:23 +0330

    v1.1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var n1 = '';
-var action = '';
-var isNew = false;
+n1 = '';
+action = '';
+isNew = false;
 
 function addNum(x) {
-    var display = document.getElementById('result');
+    display = document.getElementById('result');
     
     if (isNew) {
         display.value = '';
@@ -18,7 +18,7 @@ function addNum(x) {
 }
 
 function setOp(op) {
-    var display = document.getElementById('result');
+    display = document.getElementById('result');
     n1 = display.value;
     action = op;
     isNew = true;
@@ -31,14 +31,14 @@ function clr() {
 }
 
 function del() {
-    var display = document.getElementById('result');
+    display = document.getElementById('result');
     display.value = display.value.slice(0, -1);
 }
 
 function mathFunc(type) {
-    var display = document.getElementById('result');
-    var val = parseFloat(display.value);
-    var javab = 0;
+    display = document.getElementById('result');
+    val = parseFloat(display.value);
+    javab = 0;
 
     if (type == 'sin') {
         javab = Math.sin(val * Math.PI / 180);
@@ -57,12 +57,12 @@ function mathFunc(type) {
 }
 
 function calc() {
-    var display = document.getElementById('result');
-    var n2 = display.value;
-    var javab = 0;
+    display = document.getElementById('result');
+    n2 = display.value;
+    javab = 0;
     
-    var a = parseFloat(n1);
-    var b = parseFloat(n2);
+    a = parseFloat(n1);
+    b = parseFloat(n2);
 
     if (action == '+') {
         javab = a + b;
@@ -82,23 +82,23 @@ function calc() {
 }
 
 function doConvert() {
-    var adad = document.getElementById('numMabna').value;
-    var az = parseInt(document.getElementById('fromM').value);
-    var be = parseInt(document.getElementById('toM').value);
+    adad = document.getElementById('numMabna').value;
+    az = parseInt(document.getElementById('fromM').value);
+    be = parseInt(document.getElementById('toM').value);
     
     if(adad == "") {
         alert("لطفا عدد وارد کنید");
         return;
     }
 
-    var temp = parseInt(adad, az); 
-    var natije = temp.toString(be); 
+    temp = parseInt(adad, az); 
+    natije = temp.toString(be); 
 
     document.getElementById('resMabna').innerHTML = "جواب: " + natije.toUpperCase();
 }
 
 function toggleHelp() {
-    var box = document.getElementById('help-text');
+    box = document.getElementById('help-text');
     if (box.style.display === 'none') {
         box.style.display = 'block';
     } else {
